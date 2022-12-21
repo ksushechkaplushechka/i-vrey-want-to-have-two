96d62defb750248ac198739576a464b1fe3e0707
ксюшечкаплюшечка <isip_k.d.porfiryeva@mpt.ru>
2022-12-21 20:18:22 +0300

    можно два

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿DriveInfo[] drives = DriveInfo.GetDrives();
+
+foreach (DriveInfo drive in drives)
+{
+    Console.WriteLine($"Название: {drive.Name}");
+    Console.WriteLine($"Тип: {drive.DriveType}");
+    if (drive.IsReady)
+    {
+        Console.WriteLine($"Объем диска: {drive.TotalSize}");
+        Console.WriteLine($"Свободное пространство: {drive.TotalFreeSpace}");
+        Console.WriteLine($"Метка диска: {drive.VolumeLabel}");
+    }
+    Console.WriteLine();
+}
